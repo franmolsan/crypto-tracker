62957e6d20528eba1d96ebcd80859f92ce895a65
franmolsan <franmolsan12@gmail.com>
2024-03-10 18:31:42 +0100

    Add error handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,16 @@
 
 import { useState } from "react";
 
+type err = {
+  message: string
+}
+
+type Balances = {
+  confirmedBalance: number;
+  unconfirmedBalance: number;
+  totalBalance: number;
+};
+
 const getURL = (address: string) => `https://mempool.space/api/address/${address}`;
 
 const getWalletData = async (event: any, address: string) => {
@@ -16,7 +26,7 @@ const getWalletData = async (event: any, address: string) => {
 	}
 };
 
-const calculateWalletBalance = (data: any, address: string): { confirmedBalance: number; unconfirmedBalance: number; totalBalance: number; } => {
+const calculateWalletBalance = (data: any, address: string): Balances => {
   const { chain_stats, mempool_stats } = data;
 
   const confirmedBalance = chain_stats.funded_txo_sum - chain_stats.spent_txo_sum;
@@ -38,13 +48,20 @@ const calculateWalletBalance = (data: any, address: string): { confirmedBalance:
 
 export default function Home() {
   const [input, setInput] = useState('')
-  const [walletBalance, setWalletBalance] = useState<{ confirmedBalance: number; unconfirmedBalance: number; totalBalance: number; } | null>(null);
+  const [walletBalance, setWalletBalance] = useState<Balances | null>(null);
+  const [error, setError] = useState<err | null>(null);
   const [loading, setLoading] = useState(false);
 
   const showWalletBalance = async(event: any, input: string) =>{
+    setWalletBalance(null);
     setLoading(true);
+    setError(null);
+
     const walletData = await getWalletData(event as any, input);
-    const walletBalance = walletData ? calculateWalletBalance(walletData, input) : null;
+    const err = walletData ? null : { message: 'Invalid wallet address'};
+    setError(err);
+
+    const walletBalance = err ? null: calculateWalletBalance(walletData, input);
     setWalletBalance(walletBalance);
     setLoading(false);
   }
@@ -53,12 +70,13 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
     <form className='flex'>
-        <input className='shadow-inner rounded-l p-2 flex-1' id='wallet' aria-label='wallet address' placeholder='Enter the wallet address' value={input} onChange={e => setInput(e.target.value)}/>
+        <input className='shadow-inner text-black rounded-l p-2 flex-1' id='wallet' aria-label='wallet address' placeholder='Enter the BTC wallet address' value={input} onChange={e => setInput(e.target.value)}/>
         <button className='bg-blue-600 hover:bg-blue-700 duration-300 text-white shadow p-2 rounded-r' type='submit' onClick={(event) => showWalletBalance(event as any, input)}>
-          Check Amount
+          Check BTC Amount
         </button>
     </form>
-      { loading && <div>Loading...</div>}
+      { loading && <div className="justify-end pt-10">Loading...</div>}
+      { error && <div className="justify-end pt-10">{`❌ ${error.message}`}</div>}
       {walletBalance && 
       (<div className="justify-end pt-10">
         <li className='list-disc'>Confirmed Balance: {walletBalance.confirmedBalance} BTC</li> 
